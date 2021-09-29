560ce7bedc917d41c38d44ba1254d4d14b876e63
josekernel2010 <joseoca2010@hotmail.com>
2021-09-29 01:01:26 -0300

    first commit

---
+++ src/main/java/com/gestion_usuario/gestion/GestionApplication.java
@@ -0,0 +1,13 @@
+package com.gestion_usuario.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestion_usuario/gestion/GestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion_usuario.gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
